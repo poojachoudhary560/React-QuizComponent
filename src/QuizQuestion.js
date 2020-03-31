768620700e89d022c902c4fac1bdd437bb911e33
Pooja Choudhary <pooja.choudhary@nagarro.com>
2020-03-31 11:48:00 +0530

    added QuizQuestion component

---
+++ src/QuizQuestion.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class QuizQuestion extends Component {
+    render() {
+        return (
+            <main>
+                <section>
+                <p>
+                    {this.props.quiz_question.instruction_text}
+                // instruction text goes here
+                </p>
+                </section>
+                <section className="buttons">
+                <ul>
+                    //quiz question button logic goes here
+                    <li>
+                        {this.props.quiz_question.answer_options[0]}
+                    </li>
+                </ul>
+                </section>
+            </main>
+        )
+    }
+}
+
+export default QuizQuestion
