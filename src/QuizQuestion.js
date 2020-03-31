72fdee919e77cb4da2c071f1b8fd97846a56f1f1
Pooja Choudhary <pooja.choudhary@nagarro.com>
2020-03-31 13:05:11 +0530

    fixed index

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -17,7 +17,7 @@ class QuizQuestion extends Component {
                 <section className="buttons">
                 <ul>
                     {
-                        this.props.quiz_question.answer_options.map((index, answer_option) => {
+                        this.props.quiz_question.answer_options.map((answer_option, index) => {
                             return <QuizQuestionButton
                             key={index}
                             button_text={answer_option}
