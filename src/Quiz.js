d81588130923eb83fff9acbbeccaf80851545ce4
Pooja Choudhary <pooja.choudhary@nagarro.com>
2020-04-01 11:09:50 +0530

    aaded reset handler

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,6 +11,11 @@ class Quiz extends Component {
             quiz_position: 1
         }
     }
+    handleResetClick() {
+        this.setState({
+            quiz_position: 1
+        })
+    }
     showNextQuestion() {
         this.setState({
             quiz_position : this.state.quiz_position + 1
@@ -20,7 +25,7 @@ class Quiz extends Component {
         const isQuizEnd = ((this.state.quiz_position - 1) === (quizData.quiz_questions.length)) ? true : false
         return (
                 <Fragment>
-                    {isQuizEnd && <QuizEnd />}
+                    {isQuizEnd && <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/>}
                     {!isQuizEnd &&
                         <QuizQuestion
                         quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
